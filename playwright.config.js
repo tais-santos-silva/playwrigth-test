d7206f6a60b37b378cb4abb10b9ec707e5b1d3d9
tais-santos-silva <ts361175@gmail.com>
2024-09-30 20:34:15 -0300

    feature/video

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ module.exports = defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    video: 'on-first-retry',
+    video: 'on',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
